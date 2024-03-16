cf0dcdab163db3867406889b83b370c9152a7cd6
AnouarBarouchi <anouar1996ba@gmail.com>
2024-03-16 22:09:19 +0100

    mission

--- Modules/Order/Database/Migrations/2024_03_16_211159_create_missions_table.php
+++ Modules/Order/Database/Migrations/2024_03_16_211159_create_missions_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('missions', function (Blueprint $table) {
-            $table->id();
-            $table->string('code')->unique();
-            $table->foreignId('order_id');
-            $table->foreignId('driver_id');
-            $table->string('status')->default('pending');
-            $table->timestamps();
-        });
+        // Schema::create('missions', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->string('code')->unique();
+        //     $table->foreignId('order_id');
+        //     $table->foreignId('driver_id');
+        //     $table->string('status')->default('pending');
+        //     $table->timestamps();
+        // });
     }
 
     /**
