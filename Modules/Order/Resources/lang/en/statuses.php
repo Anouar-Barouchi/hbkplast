cc5d4d77627181171cfb2446ef1ef957823e9635
AnouarBarouchi <anouar1996ba@gmail.com>
2024-03-16 14:49:46 +0100

    fhdsmqksljhdfmkjqshd

--- Modules/Order/Resources/lang/en/statuses.php
+++ Modules/Order/Resources/lang/en/statuses.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'canceled' => 'Canceled',
     'pending' => 'Pending',
     'accepted' => 'Accepted',
     'charged' => 'Charged',
     'on_road' => 'On Road',
     'discharged' => 'Discharged',
     'finished' => 'Finished',
+    'canceled' => 'Canceled',
 ];
