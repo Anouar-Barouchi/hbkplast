b3079364b5d27fdaf92ac49f7badd3baa34bd530
AnouarBarouchi <anouar1996ba@gmail.com>
2024-04-18 14:29:51 +0100

    lsnqd

--- Modules/Product/Resources/views/admin/products/tabs/general.blade.php
+++ Modules/Product/Resources/views/admin/products/tabs/general.blade.php
@@ -7,7 +7,7 @@
         {{ Form::select('categories', trans('product::attributes.categories'), $errors, $categories, $product, ['class' => 'selectize prevent-creation', 'multiple' => true]) }}
         {{-- {{ Form::select('tax_class_id', trans('product::attributes.tax_class_id'), $errors, $taxClasses, $product) }} --}}
         {{ Form::select('tags', trans('product::attributes.tags'), $errors, $tags, $product, ['class' => 'selectize prevent-creation', 'multiple' => true]) }}
-        {{-- {{ Form::checkbox('is_virtual', trans('product::attributes.is_virtual'), trans('product::products.form.the_product_won\'t_be_shipped'), $errors, $product) }} --}}
+        <input type="hidden" name="is_virtual" value="0">
         {{ Form::checkbox('is_active', trans('product::attributes.is_active'), trans('product::products.form.enable_the_product'), $errors, $product, ['checked' => true]) }}
     </div>
 </div>
