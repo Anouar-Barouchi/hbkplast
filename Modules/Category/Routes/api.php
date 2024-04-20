10d188b87a3e5435a89da6dfad58ee134932250a
AnouarBarouchi <anouar1996ba@gmail.com>
2024-04-20 21:06:00 +0100

    cache

--- Modules/Category/Routes/api.php
+++ Modules/Category/Routes/api.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('categories', 'CategoryController@index')->name('categories.index');
+Route::get('categories', 'CategoryController@index');
 
 Route::get('categories/{category}/products', 'CategoryProductController@index')->name('categories.products.index');
