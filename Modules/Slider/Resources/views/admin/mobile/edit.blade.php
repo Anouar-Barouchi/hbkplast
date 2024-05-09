742d325f9bba9e82fb24f1758414dd753f41ffad
AnouarBarouchi <anouar1996ba@gmail.com>
2024-05-09 21:42:43 +0100

    sliders

--- Modules/Slider/Resources/views/admin/mobile/edit.blade.php
+++ Modules/Slider/Resources/views/admin/mobile/edit.blade.php
@@ -9,12 +9,13 @@
 @endcomponent
 
 @section('content')
-    <form method="POST" action="{{ route('admin.sliders.update', $slider) }}" class="form-horizontal" id="slider-edit-form" novalidate>
+    <form method="POST" action="{{ route('admin.sliders.update', $slider) }}" class="form-horizontal" enctype="multipart/form-data" id="slider-edit-form" novalidate>
         {{ csrf_field() }}
         {{ method_field('put') }}
+        <div class="media-picker-divider"></div>
         @include('media::admin.image_picker.multiple', [
             'title' => trans('product::products.form.additional_images'),
-            'inputName' => 'files[additional_images][]',
+            'inputName' => 'files[images][]',
             'files' => $slider->images,
         ])
         
